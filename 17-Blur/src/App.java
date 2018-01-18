d3906a73d3ed3e451fa693caf5d3fe8873c7bb1c
Ramon Lacava <ramonrune@gmail.com>
2018-01-18 20:35:32 -0200

    Blur

---
+++ 17-Blur/src/App.java
@@ -0,0 +1,50 @@
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.RotatedRect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+public class App {
+
+	public static void main(String[] args) throws IOException {
+	
+		
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		
+		Mat src = Imgcodecs.imread("Lighthouse.jpg");
+		
+		
+		Mat dst = new Mat();
+		
+		Size size = new Size(45, 45);
+		Point point = new Point(20, 30);
+		
+		Imgproc.blur(src, dst, size, point, Core.BORDER_DEFAULT);
+		
+		
+		Imgcodecs.imwrite("blur.jpg", dst);
+		
+
+	
+		
+		
+	}
+}
